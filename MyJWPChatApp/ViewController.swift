1896c5376a6e924c0f876c8191faf64494e40c57
Amitai Blickstein <blickstein@gmail.com>
2020-11-01 21:52:11 -0500

    Initial Commit

---
+++ MyJWPChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyJWPChatApp
+//
+//  Created by Amitai Blickstein on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
