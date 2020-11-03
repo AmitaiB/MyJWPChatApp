e271a752c136e01c02b5bdc547cde4045beb1f68
Amitai Blickstein <blickstein@gmail.com>
2020-11-03 09:36:55 -0500

    Adds FirebaseManager

---
+++ MyJWPChatApp/Backend/FirebaseManager.swift
@@ -0,0 +1,15 @@
+//
+//  FirebaseManager.swift
+//  MyJWPChatApp
+//
+//  Created by Amitai Blickstein on 11/3/20.
+//
+
+import Foundation
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
+
+class FirebaseManager {
+    
+}
