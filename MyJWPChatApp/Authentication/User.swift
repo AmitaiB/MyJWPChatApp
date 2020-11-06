c9980d36b06bc26e155fc9e2909aa352d9ea79d1
Amitai Blickstein <blickstein@gmail.com>
2020-11-05 23:29:01 -0500

    Adds `User` struct

---
+++ MyJWPChatApp/Authentication/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  MyJWPChatApp
+//
+//  Created by Amitai Blickstein on 11/5/20.
+//
+
+import Foundation
+
+struct User {
+    var username: String
+    var email: String
+    var uid: String
+    var profileImageUrl: String
+}
