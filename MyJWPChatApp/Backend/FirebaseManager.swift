0d03bae857cd03a92bbe748cf003e61f98634a39
Amitai Blickstein <blickstein@gmail.com>
2020-11-10 17:12:54 -0500

    Adds Map to Result type

--- MyJWPChatApp/Backend/FirebaseManager.swift
+++ MyJWPChatApp/Backend/FirebaseManager.swift
@@ -17,14 +17,13 @@ class FirebaseManager {
     
     static func login(email: String, password: String, completion: @escaping (_ success: Result<AuthDataResult, Error>) -> Void) {
         
-        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
-            if let error = error {
-                print(error.localizedDescription)
-                completion(.failure(error))
-            } else
-            if let authResult = result {
-                currentUser   = authResult.user.asLocalUserObj
-                completion(.success(authResult))
+        Auth.auth().signIn(withEmail: email, password: password) {
+            switch $0 {
+                case .success(let data):
+                    currentUser   = data.user.asLocalUserObj
+                    completion(.success(data))
+                case .failure(let error):
+                    completion(.failure(error))
             }
         }
     }
@@ -38,4 +37,28 @@ extension FirebaseAuth.User {
              uid: uid,
              profileImageUrl: photoURL?.absoluteString ?? "profile image url not set")
     }
+}
+
+extension Auth {
+    /// Internally, calls the ObjC-backed version that passes back an Optional/Error, and maps it to a Result type.
+    public func signIn(withEmail email: String,
+                       password: String,
+                       completion: ((Result<AuthDataResult, Error>) -> Void)?)
+    {
+        signIn(withEmail: email, password: password) { (data, error) in
+            
+            if let completion = completion {
+                completion(self.resultFrom(response: data, error))
+            }
+        }
+    }
+    
+    /// Maps the Firebase Optional/Error response to a Swift 4+ Result type (`Result<AuthDataResult, Error>`).
+    private func resultFrom(response: AuthDataResult?, _ error: Error?) -> Result<AuthDataResult, Error> {
+        if let response = response { return .success(response) }
+        
+        return .failure(error ?? LoginError.unknownError)
+    }
+    
+    enum LoginError: Error { case unknownError }
 }
