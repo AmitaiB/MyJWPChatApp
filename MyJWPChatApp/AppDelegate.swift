71b6318ee035f193262ffb5c489a3ac41d3ab592
Amitai Blickstein <blickstein@gmail.com>
2020-11-25 19:30:54 -0500

    Adds and ignores SecureKeys.swift
    sq

--- MyJWPChatApp/AppDelegate.swift
+++ MyJWPChatApp/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        JWPlayerController.setPlayerKey(Secure.Keys.JWPv3Key)
+        
         FirebaseApp.configure()
         // Set category to playback
         do {
